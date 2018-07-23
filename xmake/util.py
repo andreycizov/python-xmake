e3bb0f82499453be761a4be46cb1670553dcaeee
Andrey Cizov <acizov@gmail.com>
2018-07-23 13:34:40 +0100

    UTIL: better logging

--- xmake/util.py
+++ xmake/util.py
@@ -1,13 +1,16 @@
 import inspect
 import sys
 
+from attr import dataclass
+from typing import Callable, Any, Dict
+
 
 def _get_outer_frames(frame, context=1, full_impl=True):
     """Get a list of records for a frame and all higher (calling) frames.
 
     Each record contains a frame object, filename, line number, function
     name, a list of lines of context, and index within the context."""
-    #framelist = []
+    # framelist = []
     while frame:
         if full_impl:
             frameinfo = (frame,) + inspect.getframeinfo(frame, context)
@@ -20,7 +23,7 @@ def _get_outer_frames(frame, context=1, full_impl=True):
 
         frame = frame.f_back
 
-    #return framelist
+    # return framelist
 
 
 def _get_stack(context=1):
@@ -34,7 +37,7 @@ def _iter_idx(iter_obj, nth):
             return x
 
 
-def _get_caller(depth=2, stdlib_impl=False) -> inspect.FrameInfo:
+def _get_caller(depth=2, stdlib_impl=True) -> inspect.FrameInfo:
     """Get caller frame"""
     if stdlib_impl:
         caller = inspect.stack()[depth]
@@ -67,22 +70,36 @@ def _make_cell(value):
     return rtn
 
 
-def _enclosed(fn, locals_or_globals_idk=None):
-    """Transform a function referencing closure variables into a callable function with arguments"""
-    co_freevars = fn.__code__.co_freevars
+@dataclass(repr=False)
+class EnclosedFree:
+    fn: Callable
+    clos_globals: Dict[str, Any] = None
 
-    if locals_or_globals_idk is None:
-        locals_or_globals_idk = {}
+    def __repr__(self):
+        fn = self.fn.__code__.co_filename if hasattr(self.fn.__code__, 'co_filename') else ''
+        ln = self.fn.__code__.co_firstlineno if hasattr(self.fn.__code__, 'co_firstlineno') else ''
+        return f'"{fn}:{ln}"'
 
-    def _enclosed_wrapper(*args):
-        assert len(args) == len(co_freevars), (args, co_freevars)
+    @property
+    def co_freevars(self):
+        return self.fn.__code__.co_freevars
+
+    def __call__(self, *args):
+        assert len(args) == len(self.co_freevars), (args, self.co_freevars)
 
         closure_vars = tuple(_make_cell(arg) for arg in args)
 
         defaults = None
 
-        new_fn = type(fn)(fn.__code__, locals_or_globals_idk, fn.__name__, defaults, closure_vars)
+        new_fn = type(self.fn)(self.fn.__code__, self.clos_globals, self.fn.__name__, defaults, closure_vars)
 
         return new_fn()
 
-    return _enclosed_wrapper
+
+def _enclosed(fn, clos_globals=None):
+    """Transform a function referencing closure variables into a callable function with arguments"""
+
+    if clos_globals is None:
+        clos_globals = {}
+
+    return EnclosedFree(fn, clos_globals)
